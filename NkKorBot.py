64dcf813725551afed5ff7659425beda63406dd0
NkKor <coder.zor@yandex.ru>
2024-09-12 17:00:32 +0700

    Первично разработан эхо-бот, выполнены дз, кроме калькулятора

--- NkKorBot.py
+++ NkKorBot.py
@@ -3,8 +3,8 @@ import logging
 import sys
 import os
 import dotenv
-from aiogram import Bot, Dispatcher
-from aiogram.filters import CommandStart
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 
 
@@ -15,14 +15,39 @@ dp = Dispatcher()
 
 @dp.message(CommandStart())
 async def start(message:Message):
-    await message.answer(f"Hi {message.from_user.full_name}")
+    await message.answer(f"Приветствую тебя, {message.from_user.full_name}\n"
+                         f"Вот комманды, доступные тебе:\n"
+                         f"/start - запустит\ перезапустит бот\n"
+                         f"/get_id - сообщит id текущего чата\n"
+                         f"/help - выведет все доступные комманды\n")
+
+@dp.message(Command("get_id"))
+async def get_id(message:Message):
+    chat_id = message.chat.id
+    await message.answer(str(chat_id))
+
+@dp.message(Command("help"))
+async def help(message:Message):
+    await message.answer(f"Вот комманды, доступные тебе:\n"
+                         f"/start - запустит\ перезапустит бот\n"
+                         f"/get_id - сообщит id текущего чата\n"
+                         f"/help - выведет все доступные комманды\n")
 
 @dp.message()
 async def handle_message(message:Message):
-    await message.answer(f"Your message {message.text}")
+    # Проверяем, содержит ли сообщение искомое слово
+    if ('hi' or 'hello' or 'what’sup') in message.text.lower():
+        # Если слово найдено, отправляем ответ
+        await message.reply(f"Hey, {message.from_user.first_name}!")
+    else:
+        await message.reply(f"""Привет {message.from_user.full_name},\n
+'{message.text}' -that’s what you said. \n
+Твой языковой код - {message.from_user.language_code}""")
+
+
 
 async def main():
-    bot = Bot(token = token)
+    bot = Bot(token=token)
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
